430f6d1e464a02d59c635436938fd5b529b3057b
elfy-f <84900275+elfy-f@users.noreply.github.com>
2021-09-07 16:21:12 +0200

    Installation d'easyAdmin+ dashboard

--- src/Command/SendContactCommand.php
+++ src/Command/SendContactCommand.php
@@ -36,7 +36,9 @@ class SendContactCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $toSend = $this->contactRepository->findBy(['isSend'=> false]);
-        $adress = new Address($this->userRepository->getAdmin()->getEmail(), $this->userRepository->getAdmin()->getNom() .'' . $this->userRepository->getAdmin()->getPrenom());
+        $adress = new Address(
+            $this->userRepository->getAdmin()->getEmail(), $this->userRepository->getAdmin()->getNom() .'' .
+            $this->userRepository->getAdmin()->getPrenom());
 
         foreach ($toSend as $mail){
             $email = (new Email())
