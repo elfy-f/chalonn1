6a60c5c23b76c126cf46af54962a48915a8871f4
elfy-f <84900275+elfy-f@users.noreply.github.com>
2021-10-04 12:40:34 +0200

    parrainge

--- src/Controller/ParrainController.php
+++ src/Controller/ParrainController.php
@@ -2,19 +2,30 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ParrainController extends AbstractController
 {
+
+    private $entitymanager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entitymanager= $entityManager;
+    }
+
     /**
      * @Route("/parrain", name="parrain")
      */
     public function index(): Response
     {
+       $parrain= $this->entitymanager;
+
         return $this->render('parrain/index.html.twig', [
-            'controller_name' => 'ParrainController',
+            'parrain'=>$parrain,
         ]);
     }
 }
